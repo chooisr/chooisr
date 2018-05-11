b1691b035e925dc374bc1f679c9624d4a51b1400
chooisr <39124725+chooisr@users.noreply.github.com>
2018-05-11 08:49:42 +0000

    Add files via upload

---
+++ 8-4.py
@@ -0,0 +1,7 @@
+deg = {10:'A', 9:'A', 8:'B', 7:'C', 6:'D', 5:'F', 4:'F', 3:'F', 2:'F', 1:'F', 0:'F'}
+score = int(input("점수 : "))
+while (score >= 0 and score <= 100):
+    print(score, ":", deg[score//10])
+    score = int(input("점수 : "))
+    continue
+print("입력 가능한 점수 범위는 0~100입니다.")
